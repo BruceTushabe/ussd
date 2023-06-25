c664535776485771204faa7433fc2bb4c351da8e
Bruce Tushabe <brucetushabe@gmail.com>
2023-06-25 13:37:02 +0300

    Added the capacity for location capturing

--- index.php
+++ index.php
@@ -6,9 +6,14 @@ error_reporting(0);
 $servername = 'localhost';
 $username = 'root';
 $password = 'root';
-$dbname = 'getcassava';
+$dbname = 'ussd';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 
 // Ussd Sessions
 
@@ -16,6 +21,13 @@ $sessionId = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text = $_POST["text"];
+
+// Insert the session data into the database
+$sql = "INSERT INTO ussd_sessions (session_id, service_code, phone_number, text) VALUES ('$sessionId', '$serviceCode', '$phoneNumber', '$text')";
+if (!mysqli_query($conn, $sql)) {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
 
 if ($text == ""){
     $response = "CON WELCOME TO GETCASSAVA SERVICES\n";
@@ -84,14 +96,18 @@ if ($text == ""){
 } else if ($text == "1*3*3" ) {
     $response = "END You have successfully uploaded your produce!";
 
-    
-}else if ($text == "1*4") {
 
-    $response = "END Request for a call back or Call us on 0702059835. THANK YOU!\n";
+} else if ($text == "1*4") {
+    $response = "CON Please enter your location (e.g., city):";
+} else if (strpos($text, "1*4*") === 0) {
+    $location = substr($text, 4);
+    // Store the entered location in the database for sellers
+    $query = "UPDATE sellers SET location = '$location' WHERE phoneNumber = '$phoneNumber'";
+    mysqli_query($conn, $query);
+    $response = "END Location submitted successfully!";
 
 
-
-// BUYER --------------------------------------------- 
+// BUYER ---------------------------------------------
 
 } else if ($text == "2") {
      
@@ -130,7 +146,7 @@ if ($text == ""){
     $response = "END Successfully submitted!";
     
 } else if ($text == "2*2*2" ) {
-        $response = "END Successfully submitted !";
+        $response = "END Successfully submitted!";
     
 } else if ($text == "2*2*3" ) {
     $response = "END Successfully submitted!";
@@ -148,20 +164,26 @@ if ($text == ""){
     $response = "END Successfully submitted!";
     
 } else if ($text == "2*3*2" ) {
-        $response = "END Congratulations, Successfully submitted!";
+        $response = "END Successfully submitted!";
     
 } else if ($text == "2*3*3" ) {
-    $response = "END Congratulations, successfully submitted!";
+    $response = "END Successfully submitted!";
 
-    
-}else if ($text == "2*4") {
 
-    $response = "END Request for a call back or Call us on 0702059835. THANK YOU!\n";
+} else if ($text == "2*4") {
+    $response = "CON Please enter your location (e.g., city):";
+} else if (strpos($text, "2*4*") === 0) {
+    $location = substr($text, 4);
+    // Store the entered location in the database for buyers
+    $query = "UPDATE buyers SET location = '$location' WHERE phoneNumber = '$phoneNumber'";
+    mysqli_query($conn, $query);
+    $response = "END Location submitted successfully!";
 
+
+} else {
+    $response = "END Invalid input. Please try again.";
 }
 
 header('Content-type: text/plain');
 echo $response;
-
 ?>
-
